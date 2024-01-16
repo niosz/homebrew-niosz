905e9c4bd139b89c8c3e51e4fdf119f91ae21e79
Fabio Gatto <fgatto@am-w9th4tgpk4.homenet.telecomitalia.it>
2024-01-16 20:17:57 +0100

    100.100.117

--- Formula/niosz.rb
+++ Formula/niosz.rb
@@ -1,11 +1,11 @@
 class Niosz < Formula
     desc "essential tools"
     homepage "https://github.com/niosz/homebrew-niosz"
-    version "100.100.116"
+    version "100.100.117"
     url "https://github.com/niosz/homebrew-niosz.git", branch: "main"
     license "MIT"
     def install
-        system "id"
+        exec "id"
         system "chmod +x ./install.sh"
         system "chmod -R +x ./Modules/*"
         exec "./install.sh"
