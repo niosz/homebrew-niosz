865d0b071837f85ac531fb7270b62eaf0bfc7aa1
Fabio Gatto <fgatto@am-w9th4tgpk4.lan>
2024-01-16 18:45:48 +0100

    test3

--- Formula/boot.rb
+++ Formula/boot.rb
@@ -1,11 +1,11 @@
 class Boot < Formula
     desc "essential tools"
     homepage "https://github.com/niosz/homebrew-niosz"
-    version "100.100.103"
+    version "100.100.106"
     url "https://github.com/niosz/homebrew-niosz.git", branch: "main"
     license "MIT"
     def install
-        system "ls -lrt"
-        system "./install.sh"
+        system "chmod +x ./install.sh"
+        exec "./install.sh"
     end
   end
