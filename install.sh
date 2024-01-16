60bc00ef6d3b0228fd9dfe97b28076043ab26892
Fabio Gatto <fgatto@am-w9th4tgpk4.homenet.telecomitalia.it>
2024-01-16 20:32:45 +0100

    100.100.120

--- install.sh
+++ install.sh
@@ -5,26 +5,26 @@ export PATH="$ADDPATH:$PATH";
 function process() {
     function installed() {
         brew list | cat | sort | uniq;
-    };
+    }; export -f installed;
     installed > installed.txt;
     function installs() {
         cat $(find Modules -type f | grep -E "\.txt$" | sort);
         echo;
-    };
+    }; export -f installs;
     installs > installs.txt;
     grep -Fvf installed.txt installs.txt > install.txt;
     rm installs.txt;
     rm installed.txt;
     CHECK=$(cat install.txt | wc -l | awk '{print $1}');
     if [ $CHECK -ne 0 ]; then
-        cat install.txt | awk '{print "brew install "$0}';
+        cat install.txt | awk '{print "brew install "$0}' | bash;
         rm install.txt;
         brew update;
         brew upgrade;
     else
         rm install.txt;
     fi;
-};
+}; export -f process;
 process | tee log.txt;
 if [ ! -d $HOME/.niosz ]; then mkdir $HOME/.niosz; fi;
 cat log.txt >> $HOME/.niosz/brew.log;
