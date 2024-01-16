13d25d8eade94af6b6d0a7ddbb2d4413526991bf
Fabio Gatto <fgatto@am-w9th4tgpk4.lan>
2024-01-16 16:58:06 +0100

    first

---
+++ Formula/boot.rb
@@ -0,0 +1,9 @@
+class Boot < Formula
+    desc "essential tools"
+    homepage "https://github.com/niosz/homebrew-niosz"
+    license "MIT"
+    def install
+        system "echo '💾 installing visual studio code'"
+        system "brew install --cask visual-studio-code"
+    end
+  end
